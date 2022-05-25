57d13e6884175ebb79bfecffd0af64c1c6c787c7
Manuel Paccagnella <manuel.paccagnella@warda.it>
2022-05-25 16:35:02 +0200

    With Jackson annotations

--- src/main/scala/it/example/jacksonAnnotationBugLib/Animal.scala
+++ src/main/scala/it/example/jacksonAnnotationBugLib/Animal.scala
@@ -1,5 +1,12 @@
 package it.example.jacksonAnnotationBugLib
 
+import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
+import it.example.jacksonAnnotationBugLib.Animal.Skunk
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes(
+  Array(new JsonSubTypes.Type(value = classOf[Skunk], name = "skunk"))
+)
 sealed trait Animal
 
 object Animal {
